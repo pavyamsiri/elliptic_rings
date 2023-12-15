f5956583de5f80d1849a522088b587d9432a8d31
Pavadol Yamsiri <pavyamsiri@gmail.com>
2023-12-15 17:27:23 +1100

    Math: Test alternatives to Cephes
    To compute elliptic integrals I have adapted cephes' elliptic integral
    functions by looking at the scipy repo. However this requires proper
    attribution and licensing.
    
    C++ does offer functions to compute elliptic integrals however they do
    take in the argument k, whereas cephes takes in m = k^2. The equations I
    am using require a negative m so m = -M for some M > 0. It is seemingly
    not possible to compute this with the standard library functions.
    
    Hence it is necessary to implement these functions myself. The Carlson
    formulation seems like the most widely used approximation.

--- src/er_ring.cpp
+++ src/er_ring.cpp
@@ -25,13 +25,31 @@ ER::Ring::Ring(f64 radius, f64 eccentricity, f64 mass)
     m_scale_width = m_semi_minor;
     m_scale_height = m_semi_major;
 
+    // TODO: Should stop using cephes related functions as they need proper licensing
+    //      Migrate to calculating them by hand using the Carlson formulation as the STL
+    //      functions don't allow for imaginary k.
     // e^2
     f64 ecc_squared = m_ecc * m_ecc;
     f64 second_ecc_squared = ecc_squared / (1 - ecc_squared);
+    f64 k_formulation = std::sqrt((1 - second_ecc_squared) * (1 - second_ecc_squared));
+    std::cout << "e' = " << second_ecc_squared << std::endl;
+    std::cout << "k = " << k_formulation << std::endl;
     f64 ellipk_ecc2 = ER::ellipk(ecc_squared);
+    f64 ellipk_ecc2_standard = std::comp_ellint_1(m_ecc);
+    std::cout << "e - ellipk - Cephes = " << ellipk_ecc2 << std::endl;
+    std::cout << "e - ellipk - STL = " << ellipk_ecc2_standard << std::endl;
     f64 ellipe_ecc2 = ER::ellipe(ecc_squared);
+    f64 ellipe_ecc2_standard = std::comp_ellint_2(m_ecc);
+    std::cout << "e - ellipe - Cephes = " << ellipe_ecc2 << std::endl;
+    std::cout << "e - ellipe - STL = " << ellipe_ecc2_standard << std::endl;
     f64 ellipk_msecc2 = ER::ellipk(-second_ecc_squared);
+    f64 ellipk_msecc2_standard = std::comp_ellint_1(second_ecc_squared);
+    std::cout << "e' - ellipk - Cephes = " << ellipk_msecc2 << std::endl;
+    std::cout << "e' - ellipk - STL = " << ellipk_msecc2_standard << std::endl;
     f64 ellipe_msecc2 = ER::ellipe(-second_ecc_squared);
+    f64 ellipe_msecc2_standard = std::comp_ellint_2(second_ecc_squared);
+    std::cout << "e' - ellipe - Cephes = " << ellipe_msecc2 << std::endl;
+    std::cout << "e' - ellipe - STL = " << ellipe_msecc2_standard << std::endl;
 
     f64 radius_squared = m_radius * m_radius;
 
